34fe01e34dde9b5784b283f2f46220926ca243c2
Sajib Bhattacharjee <86997775+Sajib-Bhattacharjee@users.noreply.github.com>
2023-01-16 15:48:00 +0600

    Updated header request.

--- 5. HTTP Response__With__QueryParameter/app.js
+++ 5. HTTP Response__With__QueryParameter/app.js
@@ -5,7 +5,16 @@ app.get("/", (req, res) => {
   // res.send("<h1> Hello I'm home page.</h1>");
   // const id = req.query.id;
   // const name = req.query.name;
-  const { id, name } = req.query;
+  const { id, name } = req.query; 
   res.send(`My Id:${id},My Name:${name}`);
+  
+   //userId/:id/userName/:name
+  //const id = req.params.id;
+  //const name = req.params.name;
+
+  //const id = req.header("id");
+  //const name = req.header("name");
+   res.send(`My Id:${id},My Name:${name}`);
+  
 });
 module.exports = app;
